24a2d8644b7e31931c4518a7f8bf211ccf35b5e7
Mateusz Sójka <mati@fastmail.com>
2025-11-06 18:30:59 +0100

    Bump release to 0.8.3

--- Formula/qqqa.rb
+++ Formula/qqqa.rb
@@ -1,8 +1,9 @@
 class Qqqa < Formula
   desc "Fast, stateless LLM assistant (qq/qa)"
   homepage "https://github.com/iagooar/qqqa"
-  url "https://github.com/iagooar/qqqa/archive/refs/tags/v0.8.3.tar.gz"
-  sha256 "d40d29c9b3dafd7ec91a60959a06aa02bb14317105fbbe23758ae8e6784bdb26"
+  url "https://github.com/iagooar/qqqa/releases/download/v0.8.3/qqqa-v0.8.3-src.tar.gz"
+  sha256 "6b843e3dfe0344ca254aecd2a231402a4165780927631ba55652162b56224dff"
+  version "0.8.3"
   license "MIT"
 
   depends_on "rust" => :build
