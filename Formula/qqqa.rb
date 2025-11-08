36de5e50bd7fcffd90017304c4152a5d67bdd2a1
Mateusz Sójka <mati@fastmail.com>
2025-11-08 17:53:40 +0100

    release: v0.9.2

--- Formula/qqqa.rb
+++ Formula/qqqa.rb
@@ -1,9 +1,9 @@
 class Qqqa < Formula
   desc "Fast, stateless LLM assistant (qq/qa)"
   homepage "https://github.com/iagooar/qqqa"
-  url "https://github.com/iagooar/qqqa/archive/refs/tags/v0.9.1.tar.gz"
-  sha256 "3127d6a913c3ca2958ae4be0563bc6b3164c544000e342767f4cdce9bc0868ef"
-  version "0.9.1"
+  url "https://github.com/iagooar/qqqa/archive/refs/tags/v0.9.2.tar.gz"
+  sha256 "66a3c0563a17560ff5b1a712e9b6d74a4a4b5cc9b00b29ce5ae161e13161d44b"
+  version "0.9.2"
   license "MIT"
 
   depends_on "rust" => :build
